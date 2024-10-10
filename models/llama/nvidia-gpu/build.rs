d6b8fcc9c82652e373441b68c99f9160255476e5
YdrMaster <ydrml@hotmail.com>
2024-10-10 17:01:14 +0800

    todo: 创建 llama-nv
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

---
+++ models/llama/nvidia-gpu/build.rs
@@ -0,0 +1,9 @@
+﻿fn main() {
+    use build_script_cfg::Cfg;
+    use search_cuda_tools::find_cuda_root;
+
+    let cfg = Cfg::new("hw_detected");
+    if find_cuda_root().is_some() {
+        cfg.define();
+    }
+}
